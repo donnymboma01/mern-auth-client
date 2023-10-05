a5d2d092651bf64e2a015911ca6ec7bcc4d09661
donnymboma01 <donnymboma01@gmail.com>
2023-10-05 16:57:34 +0200

    feat: auth with google completed

--- src/components/Oauth.jsx
+++ src/components/Oauth.jsx
@@ -2,13 +2,30 @@
 import React from "react";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "@firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signinSuccess } from "../redux/user/userSlice";
 
 function Oauth() {
+  const dispatch = useDispatch();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
+      const response = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await response.json();
+      dispatch(signinSuccess(data));
       console.log(result);
     } catch (error) {
       console.log("could not login with google");
